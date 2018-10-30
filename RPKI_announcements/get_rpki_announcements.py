1496a332f93b77e6c4cdb09fdde6b900fbcb9b6b
Serena Booth <sbooth@Mit.edu>
2018-10-30 18:51:15 -0400

    Script to grab RPKI csv

---
+++ RPKI_announcements/get_rpki_announcements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import urllib
+import datetime
+
+now = datetime.datetime.now()
+f = urllib.URLopener()
+f.retrieve("http://localcert.ripe.net:8088/export.csv", now.strftime("%Y-%m-%d %H:%M") + "_RPKI_announcements.csv")
