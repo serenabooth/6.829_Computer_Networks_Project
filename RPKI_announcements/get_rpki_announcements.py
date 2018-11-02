94362f290c755ed633bec6f6542ac34100e480bc
Serena Booth <sbooth@Mit.edu>
2018-11-02 12:10:47 -0400

    RPKI announcements save

--- RPKI_announcements/get_rpki_announcements.py
+++ RPKI_announcements/get_rpki_announcements.py
@@ -5,4 +5,4 @@ import datetime
 
 now = datetime.datetime.now()
 f = urllib.URLopener()
-f.retrieve("http://localcert.ripe.net:8088/export.csv", now.strftime("%Y-%m-%d %H:%M") + "_RPKI_announcements.csv")
+f.retrieve("http://localcert.ripe.net:8088/export.csv", now.strftime("%Y-%m-%d_%H:%M") + "_RPKI_announcements.csv")
