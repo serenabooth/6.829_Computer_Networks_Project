51b11d4b99c3b50c3d02caf07154aea812d1bfa2
Serena Booth <sbooth@Mit.edu>
2018-11-24 11:05:37 -0500

    Adding some more ROAs, hierarchy measure cleaning

--- rpki_hierarchy_measure.py
+++ rpki_hierarchy_measure.py
@@ -1,4 +1,4 @@
-import csv, random, radix 
+import csv, random, radix, os
 from netaddr import *
 
 # https://netaddr.readthedocs.io/en/latest/tutorial_03.html
@@ -17,7 +17,7 @@ def read_ip_ranges_into_radix(file_to_read):
             rnode.data["issuing_as"] = row[0]
     return rtree
 
-def count_hierarchy(ip_address_tree):
+def count_hierarchy(day, ip_address_tree):
     count_number_overlaps = 0
     max_hierarchy_depth = 0
     for node in ip_address_tree.nodes():
@@ -25,15 +25,21 @@ def count_hierarchy(ip_address_tree):
         if len(hierarchy) > 1:
             count_number_overlaps += 1
             max_hierarchy_depth = max(max_hierarchy_depth, len(hierarchy))
-            print "\n\n\n"
-            print "Covered node: " + str(node.data["issuing_as"]) + ", " + str(node.prefix)
-            for ipaddr_node in hierarchy:
-                print ipaddr_node.data["issuing_as"]
-                print ipaddr_node.prefix
+            # print "\n\n\n"
+            # print "Covered node: " + str(node.data["issuing_as"]) + ", " + str(node.prefix)
+            # for ipaddr_node in hierarchy:
+            #     print ipaddr_node.data["issuing_as"]
+            #     print ipaddr_node.prefix
 
-    print max_hierarchy_depth
-    print count_number_overlaps
+    print "Data: " + str(day)
+    print "    Max heirarchy depth: " + str(max_hierarchy_depth)
+    print "    Overlapping addresses: " + str(count_number_overlaps)
 
 
-all_ip_addresses_tree = read_ip_ranges_into_radix('RPKI_announcements/2018-10-30_18:38_RPKI_announcements.csv')
-count_hierarchy(all_ip_addresses_tree)
+rpki_announcements_per_day = os.listdir('RPKI_announcements/')
+rpki_announcements_per_day.sort()
+
+for day_of_roas in rpki_announcements_per_day: 
+    if ".csv" in day_of_roas: 
+        all_ip_addresses_tree = read_ip_ranges_into_radix('RPKI_announcements/' + day_of_roas)
+        count_hierarchy(day_of_roas, all_ip_addresses_tree)
